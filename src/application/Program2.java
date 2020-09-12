3d7b1e402581cdf37a922a96d4071c2d8d04828b
CLAUDISNEI <claudisneibello@gmail.com>
2020-09-12 12:07:41 -0300

    Criado classe Program2 para teste

---
+++ src/application/Program2.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Scanner;
+
+import model.DAO.DaoFactory;
+import model.DAO.DepartmentDAO;
+import model.entities.Department;
+
+public class Program2 {
+	public static void main(String[] args) {
+		
+		DepartmentDAO departmentDAO = DaoFactory.createDepartmentDao();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("=====Teste 1: Incluindo departamento no Banco de Dados=====");
+		Department novoDepartamento = new Department(null, "Automóveis");
+		departmentDAO.insert(novoDepartamento);
+		System.out.println("Departamento criado Id: "+novoDepartamento.getId());
+				
+				
+	}
+}
